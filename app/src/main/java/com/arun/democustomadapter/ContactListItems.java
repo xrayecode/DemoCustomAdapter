93e317affa5c1429b7de2691d452e03faeb853b8
xrayecode <xrayecode@gmail.com>
2015-06-03 15:31:21 +0300

    Initial commit

---
+++ app/src/main/java/com/arun/democustomadapter/ContactListItems.java
@@ -0,0 +1,33 @@
+package com.arun.democustomadapter;
+
+public class ContactListItems {
+
+	String slno;
+	String name;
+	String phone;
+
+	public String getSlno() {
+		return slno;
+	}
+
+	public void setSlno(String slno) {
+		this.slno = slno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
